83f40a4fe48eccc5df5803c52a2a6d00510774a8
William Metcalf <metcalf_william@network.lilly.com>
2021-02-09 09:35:02 -0500

    init backend

---
+++ frontend/src/App/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Button } from "@material-ui/core";
+
+function App() {
+  return (
+    <div className="App">
+      <Button
+        onClick={() => {
+          const twitchUrl =
+            `https://id.twitch.tv/oauth2/authorize?` +
+            `client_id=${process.env.REACT_APP_TWITCH_CLIENT_ID}&` +
+            `redirect_uri=http://localhost:3000/auth&` +
+            `response_type=token+id_token&` +
+            `scope=openid`;
+          window.open(
+            twitchUrl,
+            "Authenticate with Twitch",
+            `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=500,height=700`
+          );
+        }}
+      >
+        Authenticate
+      </Button>
+    </div>
+  );
+}
+
+export default App;
